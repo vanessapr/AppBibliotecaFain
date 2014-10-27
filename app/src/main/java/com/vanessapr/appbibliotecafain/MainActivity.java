75ee6e411d376669d7369f969c5a4674e730721a
Vanessa M <vanessapr.85@gmail.com>
2014-10-27 18:21:24 -0500

    initializing elements in MainActivity

--- app/src/main/java/com/vanessapr/appbibliotecafain/MainActivity.java
+++ app/src/main/java/com/vanessapr/appbibliotecafain/MainActivity.java
@@ -2,13 +2,36 @@ package com.vanessapr.appbibliotecafain;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private EditText txtBusqueda;
+    private Button btnSeartch, btnSearchAdvanced;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtBusqueda = (EditText) findViewById(R.id.txt_busqueda);
+        btnSeartch = (Button) findViewById(R.id.btnSearch);
+        btnSearchAdvanced = (Button) findViewById(R.id.btnSearchAdvanced);
+        btnSeartch.setOnClickListener(this);
+        btnSearchAdvanced.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSearch:
+                                
+                break;
+
+            case R.id.btnSearchAdvanced:
+                break;
+        }
+
+    }
 }
