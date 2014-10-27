e1a9178db1d224afadeca37edab4273679227971
Vanessa M <vanessapr.85@gmail.com>
2014-10-27 18:26:50 -0500

    adding BooksActivity

---
+++ app/src/main/java/com/vanessapr/appbibliotecafain/BooksActivity.java
@@ -0,0 +1,16 @@
+package com.vanessapr.appbibliotecafain;
+
+
+import android.os.Bundle;
+
+
+public class BooksActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+
+    }
+
+}
