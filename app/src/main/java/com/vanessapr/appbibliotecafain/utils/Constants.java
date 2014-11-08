cca4b1b967da437bcf134f74133cb10a3a79ef9e
Vanessa M <vanessapr.85@gmail.com>
2014-11-08 07:34:19 -0500

    added Constants class

---
+++ app/src/main/java/com/vanessapr/appbibliotecafain/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.vanessapr.appbibliotecafain.utils;
+
+/**
+ * Created by Milagros on 08/11/2014.
+ */
+public final class Constants {
+
+    public static final String HOST_PDF = "http://ivica.org/bibliotecafain/";
+}
